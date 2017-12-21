0cab2211cd1729d4a850470bb2732137cbf58855
Librazy <im@librazy.org>
2017-12-21 22:51:57 +0800

    update

--- Group1Extensions.cs
+++ Group1Extensions.cs
@@ -5,10 +5,10 @@ namespace Microsoft.Extensions.DependencyInjection
 { 
     public static class Group1Extensions
     {
-        // 为每一个你写的Service写一个这样的函数，把 UserService 替换为你实现的 Service
-        public static IServiceCollection AddGroup1UserService(this IServiceCollection serviceCollection)
+        // 为每一个你写的Service写一个这样的函数，把 LoginService 替换为你实现的 Service
+        public static IServiceCollection AddGroup1LoginService(this IServiceCollection serviceCollection)
         {
-            return serviceCollection.AddScoped<IUserService, UserService>();
+            return serviceCollection.AddScoped<ILoginService, LoginService>();
         }
     }
 }
